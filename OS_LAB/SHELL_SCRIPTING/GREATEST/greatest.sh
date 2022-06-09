0b6c6c249ad312c8de25eaf11b9dabf29cf89965
JeevithaSimma <93779394+JeevithaSimma@users.noreply.github.com>
2022-06-09 15:24:58 +0530

    greatest number

---
+++ OS_LAB/SHELL_SCRIPTING/GREATEST/greatest.sh
@@ -0,0 +1,14 @@
+echo "enter the value of a b c" 
+read a
+read  b
+read  c
+if [ $a -gt $b -a $a -gt $c ]
+then
+echo  "a  is  greatest"
+elif [ $b -gt $c -a $b -gt $a ] 
+then
+echo  "b  is  greatest"
+else
+echo  "c  is  greatest"
+fi
+
